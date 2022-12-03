470e6669820d8e9b656094f9c40a33fc790f9ef1
jefc <jefc_uk@hotmail.com>
2022-12-03 21:05:44 +0000

    Add attrs to button_with_icon

--- lib/radio_tracker_web/components/form/button.ex
+++ lib/radio_tracker_web/components/form/button.ex
@@ -4,10 +4,14 @@ defmodule RadioTrackerWeb.Components.ButtonWithIcon do
   import RadioTrackerWeb.Components.Icon
 
   attr :class, :string, default: nil
+  attr :icon_name, :string, default: nil
+  attr :icon_type, :string, default: nil
+  attr :text, :string, default: nil
+  attr :type, :string, default: nil
 
   def button_with_icon(assigns) do
     ~H"""
-    <button class={ "button " <> @class }>
+    <button type={ @type } class={ "button " <> @class }>
       <span class="icon">
         <.icon name={ @icon_name } type={ @icon_type } class="icon" />
       </span>
