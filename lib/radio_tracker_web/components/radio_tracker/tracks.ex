8ac1d115664f7517161652de47539813a00e079f
jefc <jefc_uk@hotmail.com>
2022-12-30 15:17:50 +0000

    Remove old pagination files

--- lib/radio_tracker_web/components/radio_tracker/tracks.ex
+++
@@ -1,7 +0,0 @@
-defmodule RadioTrackerWeb.TracksView do
-  use RadioTrackerWeb, :html
-  alias RadioTracker.Helpers.Dates
-  alias RadioTracker.Schemas.Track
-
-  embed_templates "../templates/tracks/*"
-end
