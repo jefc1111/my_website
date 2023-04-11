f89585c7136a0f8b44458ffadcca4ea78d12d01f
jefc <jefc_uk@hotmail.com>
2023-04-11 21:14:05 +0100

    Set default date to last 7 days

--- lib/helpers/dates.ex
+++ lib/helpers/dates.ex
@@ -15,4 +15,18 @@ defmodule RadioTracker.Helpers.Dates do
       _ -> "Not recognised"
     end
   end
+
+  def default_start() do
+    Timex.now |> Timex.subtract(Timex.Duration.from_days(7)) |> format
+  end
+
+  def default_end() do
+    format(Timex.now)
+  end
+
+  defp format(date) do
+    {:ok, res} = Timex.format(date, "{YYYY}-{0M}-{D}")
+
+    res
+  end
 end
